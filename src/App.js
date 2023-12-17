03db23f43f5f14e1da98302a33a46d24ce13dd0b
Aljith KJ <kwdeathlok@gmail.com>
2023-12-17 22:05:28 +0530

    Add eslint setup and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React  from 'react'
 import './App.css'
 
 const App = () => {
+
   return (
-    <div>
-        <h1>Reactive Forge</h1>
+    <div className="container">
+      <header className="header">
+        <h1 className="title">Reactive Forge</h1>
+        <p className="description">
+          Simplifying React bundling for an optimized development experience.
+        </p>
+      </header>
+
+      <section className="main-content">
+        <div className="features">
+          <h2 className="feature-title">Key Features</h2>
+          <ul className="feature-list">
+            <li>Efficient bundling of React apps</li>
+            <li>Performance optimization</li>
+            <li>Seamless development experience</li>
+          </ul>
+        </div>
+
+        <div className="additional-content">
+          <h2 className="additional-title">Additional Information</h2>
+          <p className="additional-description">
+            This space can contain more details, FAQs, or any other relevant information about Reactive Forge.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
